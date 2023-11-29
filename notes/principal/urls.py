b114d1ef35651f6906b25d68e8604fa44ff9284f
Cristian Hernandez <cristianmanuel2304@hotmail.com>
2023-11-29 17:23:49 -0500

    urls

--- notes/principal/urls.py
+++ notes/principal/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-
+from . import views
 urlpatterns=[
+    path('',views.lista),
+    path('nota/',views.nota)
 
 ]
--- notes/principal/views.py
+++ notes/principal/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse    
 
 # Create your views here.
+def nota(request):
+    return render(request, 'nota.html')
+
+
+def lista(request):
+    return render(request,'index.html')
