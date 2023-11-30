0939e56509f60a149ae11e84069d001edc7ce957
Cristian Hernandez <cristianmanuel2304@hotmail.com>
2023-11-29 19:34:07 -0500

    htmls de prueba

--- notes/principal/views.py
+++ notes/principal/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse    
-
+from .models import Nota
 # Create your views here.
-def nota(request):
-    return render(request, 'nota.html')
+def nota(request,id):
+    title = Nota.objects.get(id==id).titulo
+    return render(request, 'nota.html',{
+        'title' : title
+
+    })
 
 
 def lista(request):
